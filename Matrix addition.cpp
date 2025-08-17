156f11b6e6fb281ff982da8d684eac14ef068e75
karan parwani <125952789+karan635@users.noreply.github.com>
2025-08-17 17:19:21 +0530

    Add files via upload

---
+++ Matrix addition.cpp
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int i,j,rows,cols;
+	int rows2,cols2;
+// taking the input of 1st matrix
+
+	printf("Enter the no. of rows that you required ");
+	scanf("%d",&rows);
+	printf("Enter the no. of cols that you required ");
+	scanf("%d",&cols);
+	int matrix[rows][cols];
+	for(i=0;i<rows;i++){
+		for(j=0;j<cols;j++){
+			printf("Enter the element at the postion [%d] [%d]  ",i+1,j+1);
+			scanf("%d",&matrix[i][j]);
+		}
+	}
+// taking input of the second matrix	
+	printf("Enter the no. of rows that you required ");
+	scanf("%d",&rows2);
+	printf("Enter the no. of cols that you required ");
+	scanf("%d",&cols2);
+	int m2[rows2][cols2];
+	for(i=0;i<rows2;i++){
+		for(j=0;j<cols2;j++){
+			printf("Enter the element at the postion [%d] [%d]  ",i+1,j+1);
+			scanf("%d",&m2[i][j]);
+		}
+	}
+// Printing of the 1st Matrix
+	printf("This 1st Matrix\n");
+	for(i=0;i<rows;i++){
+		for(j=0;j<cols;j++){
+			printf("%d ",matrix[i][j]);
+		}
+		printf("\n");
+	}
+	
+// printing the the second matrix 
+	printf("This second matrix\n");
+	for(i=0;i<rows2;i++){
+		for(j=0;j<cols2;j++){
+			printf("%d ",m2[i][j]);
+		}
+		printf("\n");
+	}
+// Addition of the matrix
+printf("This Block is For Addition\n");
+if(rows==rows2 && cols==cols2){
+	int m3[rows2][cols2];
+	for(i=0;i<rows2;i++){
+		for(j=0;j<cols2;j++){
+			m3[i][j]=matrix[i][j] *	 m2[i][j];		
+		}
+	}
+// printing the addition of the matrix 
+	for(i=0;i<rows2;i++){
+		for(j=0;j<cols2;j++){
+			printf("%d ",m3[i][j]);
+		}
+		printf("\n");
+	}
+}
+else{
+	printf("Rows and Cols are not matched\n");
+}
+
+getch();
+}
